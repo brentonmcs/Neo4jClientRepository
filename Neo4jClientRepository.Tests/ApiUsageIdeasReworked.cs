60fc7fbff21d9a18c8b4a47fa35f51106c287da5
Brenton McSweyn <bmcsweyn@betclearer.com>
2013-03-04 19:45:30 +0000

    .

--- Neo4jClientRepository.Tests/ApiUsageIdeasReworked.cs
+++ Neo4jClientRepository.Tests/ApiUsageIdeasReworked.cs
@@ -6,44 +6,39 @@ using System;
 
 namespace Neo4jClientRepository.Tests
 {
-
-
-
-    
-    class ApiUsageIdeasReworked
+    internal class ApiUsageIdeasReworked
     {
 
         // This is my reworked version of Readify Api Usage Ideas using the Repository Library
         // For the original visit  -    http://hg.readify.net/neo4jclient/src
 
 
-        void Foo()
+        private void Foo()
         {
             IGraphClient graph = new GraphClient(new Uri(""));
 
             // Based on http://wiki.neo4j.org/content/Image:Warehouse.png
 
             INeo4jRelationshipManager relationshipManager = new Neo4jRelationshipManager();
-            
+
 
             var storageLocationService = new Neo4NodeRepository<OwnedBy>(graph, relationshipManager);
-            
+
             var partsAndProductService = new Neo4NodeRepository<StoredIn>(graph, relationshipManager);
 
-          
+
 
             var frameStore = storageLocationService.UpSert(new StorageLocation {Name = "Frame Store"});
 
-            var mainStore =  storageLocationService.UpSert(new StorageLocation {Name =  "Main Store"});
+            var mainStore = storageLocationService.UpSert(new StorageLocation {Name = "Main Store"});
 
-            partsAndProductService.UpSert(new Part { Name = "Frame" }, frameStore);
+            var frame = partsAndProductService.UpSert(new Part {Name = "Frame"}, frameStore);
 
-            partsAndProductService.UpSert(new Product { Name = "Trike", Weight = 2 }, mainStore);
-            
+            partsAndProductService.UpSert(new Product {Name = "Trike", Weight = 2}, mainStore);
 
-          
 
         }
+
     }
 }
     
