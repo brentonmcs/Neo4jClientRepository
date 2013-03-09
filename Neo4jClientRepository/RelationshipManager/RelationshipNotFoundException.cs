574988fedd81149750592b79576dc628bb918f1f
Brenton McSweyn <bmcsweyn@betclearer.com>
2013-03-09 12:34:27 +0000

    Version 1

---
+++ Neo4jClientRepository/RelationshipManager/RelationshipNotFoundException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Neo4jClientRepository.RelationshipManager
+{
+    [Serializable]
+    public class RelationshipNotFoundException : Exception
+    {
+            public RelationshipNotFoundException(string message)
+            : base(message)
+        {
+
+        }
+
+        public RelationshipNotFoundException()
+        {
+            // Add any type-specific logic, and supply the default message.
+        }
+
+        public RelationshipNotFoundException(string message, Exception innerException) :
+            base(message, innerException)
+        {
+            // Add any type-specific logic for inner exceptions.
+        }
+
+        protected RelationshipNotFoundException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+            // Implement type-specific serialization constructor logic.
+        }
+    }
+}
